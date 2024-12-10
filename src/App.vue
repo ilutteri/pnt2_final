9e4011c1227af29fe34a990cc0c78a22ecbf42c6
Ignacio Lutteri <ignaciolutteri@gmail.com>
2024-12-10 20:42:11 -0300

    resolucion final

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mt-3">
+    <div class="jumbotron">
+      <h1>Programación en Nuevas Tecnologías II</h1>
+      <hr>
+      <h3>Final - Ignacio Lutteri - 10/12/2024</h3>
+      <br>
+
+    <Formulario />
+    </div>
+  </div>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
+
+<style>
+
+h3{
+  font-style: italic;
+}
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,81 @@
+<template>
+
+  <section class="src-components-formulario">
+
+    <input type="text" id="input" placeholder="Ingrese un texto aqui" v-model.trim="textoInput">
+    <br>
+    <br>
+
+    <p v-if="textoInput">Cantidad de Caracteres: {{ cantidadDeCaracteres }}</p>
+    <p v-if="textoInput">Cantidad de Vocales: {{ cantidadDeVocales }}</p>
+    <p v-if="textoInput">Texto en Mayuscula: {{ textoInput.toUpperCase() }}</p>
+    <p v-if="textoInput">Texto Invertido: {{ textoInvertido }}</p>
+   
+    <hr>
+
+    <p>Respuestas 1: C - 2:C - 3: A</p>
+  </section>
+
+</template>
+
+<script lang="js">
+
+export default {
+  name: 'src-components-formulario',
+  props: [],
+  mounted() {
+
+  },
+  data() {
+    return {
+      textoInput: null
+    }
+  },
+  computed: {
+
+    cantidadDeCaracteres() {
+      return this.textoInput ? this.textoInput.length : 0;
+    },
+
+    cantidadDeVocales() {
+      const cadenaMinuscula = this.textoInput.toLowerCase();
+      const vocales = "aeiou";
+      let contador = 0;
+
+      for (let char of cadenaMinuscula) {
+        if (vocales.includes(char)) {
+          contador++;
+        }
+      }
+      return contador;
+    },
+
+    cantidadDeVocales() {
+      const cadenaMinuscula = this.textoInput.toLowerCase();
+      const vocales = "aeiou";
+      let contador = 0;
+
+      for (let char of cadenaMinuscula) {
+        if (vocales.includes(char)) {
+          contador++;
+        }
+      }
+      return contador;
+    },
+
+    textoInvertido() {
+    return this.textoInput.split("").reverse().join("");
+}
+
+
+  }
+}
+
+</script>
+
+<style scoped lang="css">
+p {
+  font-size: 17px;
+  font-weight: 500;
+}
+</style>
